45ac44ddf0cf4a9f871ba388db9413b6302b7881
Geoffrey DeSena <geoffrey.desena@angstrom.uu.se>
2026-03-20 10:26:44 +0100

    started exercise 1

---
+++ exercises-day5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 20 10:21:11 2026
+
+@author: geoffreydesena
+"""
+
+# %% import packages
+
+import scipy as sp
+
+# %% Exercise 1: scipy
+
+# a. define a matrix
+A = [
+     [1, -2, 3],
+     [4, 5, 6],
+     [7, 1, 9]
+]
+
+# b. define b vector
+B = [1,2,3]
+
